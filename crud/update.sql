1969f761ea1d410cb51611e276f58c9f3eccd6ab
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2022-07-13 11:56:26 -0300

    feat: function update

--- crud/update.sql
+++ crud/update.sql
@@ -1,5 +1,13 @@
 -- update a value
-update person
-set gender='FEMALE'
-where id = 1;
+update person           -- update table name
+set gender='FEMALE'     -- set allows to pass an array of columns including the new values
+                        -- set puts the value in the column we pass
+where id = 1;           -- with this condition
 
+
+-- without where it would update every single row in the table
+
+-- we can update more than one field
+update person
+set first_name = 'Heloise', last_name = 'Lark', email = 'heloiselark@hotmail.com'   
+where id = 287
