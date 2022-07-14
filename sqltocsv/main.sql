b42e443d0f75c53f37090a98ea6330a2451a602e
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2022-07-14 10:12:55 -0300

    feat: saving an sql file to csv

---
+++ sqltocsv/main.sql
@@ -0,0 +1,7 @@
+-- in the psql:
+-- \copy (command_to_be_saved) to 'where_it_should_be_saved' 
+-- delimiter 'how the data is separated' (file_format_type) header(with_the_header)
+
+-- in the pg admin:
+-- after the result click on the left superior 'save results to file'
+-- and save
